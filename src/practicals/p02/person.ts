e392ea35592a5d68fd756c72d2ff48ebeefd630b
silk <sanranyywitch15@gmail.com>
2026-02-16 10:01:09 +0700

    q3-3

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,16 @@
-export class Person {}
+export class Person {
+    firstname? : string;
+    lastname? : string;
+    
+    private age? : number
+    static COUNTRY : string ="AMERICA"
+    setAge(age : number){
+        this.age = age;
+    }
+    getAge(){
+        return this.age
+    }
+    getFullName(){
+        return `${this.firstname} ${this.lastname}`
+    }
+}
